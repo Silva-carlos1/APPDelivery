4031497e6006d951a60e58f55b6bf7efbb5f7bab
Carlos <silva.carloseduardo.ds@gmail.com>
2024-03-13 21:07:20 -0300

    feat: Implement individual product pages with dynamic routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,31 @@
 import { useState, useRef } from "react";
 import { View, Text, FlatList, SectionList } from "react-native";
+import { Link } from "expo-router";
 
-import { CATEGORIES, MENU } from "@/utils/data/products"
+import { CATEGORIES, MENU } from "@/utils/data/products";
 
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
 import { Product } from "@/components/product";
 
-
 export default function Home() {
-  const [category, setCategory] = useState(CATEGORIES[0])
+  const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null)
+  const sectionListRef = useRef<SectionList>(null);
 
   function handleCategorySelect(selectedCategory: string) {
-    setCategory(selectedCategory)
+    setCategory(selectedCategory);
 
     const sectionIndex = CATEGORIES.findIndex(
-      (category) => category === selectedCategory 
-      )
-    
-    if(sectionListRef.current) {
+      (category) => category === selectedCategory
+    );
+
+    if (sectionListRef.current) {
       sectionListRef.current.scrollToLocation({
         animated: true,
         sectionIndex,
-        itemIndex: 0
-      })
+        itemIndex: 0,
+      });
     }
   }
 
@@ -33,34 +33,41 @@ export default function Home() {
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={5} />
 
-      <FlatList 
+      <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({item}) => (
-          <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
         )}
         horizontal
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
       />
 
       <SectionList
-      ref={sectionListRef}
-      sections={MENU}
-      keyExtractor={(item) => item.id}
-      stickySectionHeadersEnabled={false}
-      renderItem={({ item }) => (
-        <Product data={item} />
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
         )}
-        renderSectionHeader={({section: { title }}) => (
-          <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
-          )}
-          className="flex-1 p-5"
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          />
-          
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
     </View>
   );
 }
