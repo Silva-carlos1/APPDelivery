61fc82c9187abbfcc90bda4b9c81f95768148f23
Carlos <silva.carloseduardo.ds@gmail.com>
2024-03-18 21:03:20 -0300

    feat: Add initial shopping cart functionality

---
+++ src/app/cart.tsx
@@ -0,0 +1,71 @@
+import { View, Text, ScrollView } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { useCartStore } from "@/stores/card-store";
+
+import { formatCurrency } from "@/utils/functions/format-currency";
+
+import { Header } from "@/components/header";
+import { Product } from "@/components/product";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { LinkButton } from "@/components/link-button";
+
+export default function Cart() {
+  const cartStore = useCartStore();
+
+  const total = formatCurrency(
+    cartStore.products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    )
+  );
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Seu carrinho" />
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        extraHeight={100}
+        >
+        <ScrollView>
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio
+              </Text>
+            )}
+
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
+
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+              <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/" />
+      </View>
+    </View>
+  );
+}
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,9 @@
 import { View, Image, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
 import { Feather } from "@expo/vector-icons";
+import { Redirect } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+
+import { useCartStore } from "@/stores/card-store";
 
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
@@ -9,10 +12,22 @@ import { Button } from "@/components/button";
 import { LinkButton } from "@/components/link-button";
 
 export default function Product() {
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
+  function handleAddToCart() {
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
+  }
   return (
     <View className="flex-1">
       <Image
@@ -21,7 +36,9 @@ export default function Product() {
         resizeMode="cover"
       />
 
+
       <View className="p-5 mt-8 flex-1">
+      <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
@@ -41,7 +58,7 @@ export default function Product() {
       </View>
 
       <View className="p-5 pb-8 gap-5">
-        <Button>
+        <Button onPress={handleAddToCart}>
           <Button.Icon>
             <Feather name="plus-circle" size={20} />
           </Button.Icon>
